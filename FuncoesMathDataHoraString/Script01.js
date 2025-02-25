c5e7eb45afe4edb2127acec7d1ce933418e1e14a
donabertilucia <sintrifugarlenda@gmail.com>
2025-02-25 08:03:56 -0300

    25.02.25

--- FuncoesMathDataHoraString/Script01.js
+++ FuncoesMathDataHoraString/Script01.js
@@ -4,3 +4,18 @@ let agora = new Date();
 console.log(agora);
 console.log(agora.toDateString());
 console.log(agora.toLocaleDateString());
+
+//Get
+console.log(agora.getFullYear()); //Mostrar ano
+
+//Set
+agora.setFullYear(2030);
+console.log(agora.toLocaleDateString()); //25/02/2030
+
+
+//Operações de Data e Hora
+let data1 = new Date("2025-02-04");
+let data2 = new Date("2025-12-17");
+
+let diferenca = data2-data1;
+console.log(diferenca/(1000*60*60*24)); //Calcular em Dias
